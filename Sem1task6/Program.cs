214911781ad3400bbee10ed03197c4aafa471e12
Ekaterina <mishinaekaterin@gmail.com>
2022-12-06 15:16:59 +0400

    Added task 6

---
+++ Sem1task6/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+* 4 -> да
+* -3 -> нет
+* 7 -> нет
+*/
+//main
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine(number + " -> " + (isEven(number) ? "да" : "нет"));
+
+//methods
+
+bool isEven(int number){
+    return number % 2 == 0;
+}
