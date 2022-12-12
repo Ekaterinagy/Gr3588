1b9910a3bb0b4de4db1c28db9b5b952bfc80545b
Ekaterina <mishinaekaterin@gmail.com>
2022-12-12 19:45:24 +0400

    Added task 19

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+string Number = ReadData("Введите пятизназное число:"); 
+char[] NumberArray = Number.ToCharArray();
+
+Array.Reverse(NumberArray);
+
+string Polindrom = new string (NumberArray);
+
+if (Number.Equals(Polindrom))
+ {
+   PrintData("да", Number);
+
+}
+else {
+    PrintData("нет",Number);
+}
+
+string ReadData(string num)
+{
+    Console.WriteLine(num);
+    return Console.ReadLine()??"0";
+}
+void PrintData(string res,string Number) 
+{    
+     Console.WriteLine(Number + " -> " + res); 
+} 
