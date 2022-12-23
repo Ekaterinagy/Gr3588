43cc047059a94330f896eac5294bfb587e74370e
Ekaterina <mishinaekaterin@gmail.com>
2022-12-23 09:52:11 +0400

    Added task 41*,43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,146 @@
+﻿/*№ 41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл
+пользователь.
+	0, 7, 8, -2, -2 -> 2
+	-1, -7, 567, 89, 223-> 3
+* Пользователь вводит число нажатий, затем программа следит за нажатиями и
+выдает сколько чисел больше 0 было введено.
+*/
+
+//---константы---
+//количество символов, которые ожидается вводить с клавиатуры
+int keysCount = 10;
+int minus = 10;
+string minusStr = "-";
+//Вводить только положительные числа, иначе выводить все вкючая со знаком минус
+bool onlyPositive = true;
+//словарь символов
+ConsoleKey[] numbers = {
+ConsoleKey.D0,
+ConsoleKey.D1,
+ConsoleKey.D2,
+ConsoleKey.D3,
+ConsoleKey.D4,
+ConsoleKey.D5,
+ConsoleKey.D6,
+ConsoleKey.D7,
+ConsoleKey.D8,
+ConsoleKey.D9,
+ConsoleKey.OemMinus
+};
+ConsoleKey[] numbersPad = {
+ConsoleKey.NumPad0,
+ConsoleKey.NumPad1,
+ConsoleKey.NumPad2,
+ConsoleKey.NumPad3,
+ConsoleKey.NumPad4,
+ConsoleKey.NumPad5,
+ConsoleKey.NumPad6,
+ConsoleKey.NumPad7,
+ConsoleKey.NumPad8,
+ConsoleKey.NumPad9,
+ConsoleKey.Subtract
+};
+//---решение---
+Console.WriteLine("Наберите символы: ");
+ConsoleKeyInfo[] data = ReadData(keysCount);
+PrintData(data);
+
+//---методы---
+//считывает нажатия клавиш keysCount раз
+ConsoleKeyInfo[] ReadData(int keysCount)
+{
+ConsoleKeyInfo[] data = new ConsoleKeyInfo[keysCount];
+for (int i = 0; i < keysCount; i++)
+{
+ConsoleKeyInfo keyInf = Console.ReadKey();
+
+data[i] = keyInf;
+}
+return data;
+}
+
+//выводит все числа из массива символов клавиатуры
+void PrintData(ConsoleKeyInfo[] data)
+{
+string result = "";
+
+//флаг начала последовательности цифр
+bool nextNumber = false;
+//флаг для учета первого знака перед цифрой
+bool isAfterMinus = false;
+string temp = "";
+for (int i = 0; i< data.Length; i++)
+{
+
+int digit = GetDigit(data[i].Key);
+ 
+   // для случаев "abc-"
+if (IsMinus(data[i].Key) && nextNumber == false)
+{
+
+isAfterMinus = true;
+}
+// для случаев "-1" или "-0"
+else if (isAfterMinus && digit !=-1)
+{
+//пропуск цифры 0 перед числом
+ if (digit != 0)
+ {
+temp += minusStr + digit;
+isAfterMinus = false;
+nextNumber = true;
+ }
+}
+// для случаев когда нет минуса и пишем первую цифру отличную от нуля
+else if (nextNumber == false && digit >0 && !isAfterMinus)
+{
+temp+=digit;
+nextNumber = true;
+}
+// для случаев когда число уже начали писать
+else if (nextNumber == true && digit !=-1)
+{
+temp+= digit;
+}
+// для случаев когда вводили цифры и пришла не цифра "12345A" или "12345-"
+else if (nextNumber == true && digit == -1)
+{
+if (!temp.Substring(0,1).Equals(minusStr) || !onlyPositive)
+
+{
+
+result+=temp+"\n";
+}
+
+temp = "";
+nextNumber = false;
+if (IsMinus(data[i].Key))
+{
+isAfterMinus = true;
+}
+}
+}
+if (temp.Length > 0 && !temp.Substring(0,1).Equals(minusStr) || !onlyPositive)
+{
+result += temp;
+}
+Console.WriteLine("Вывод: \n" + result);
+}
+
+//является ли введенный символ минусом
+bool IsMinus(ConsoleKey key)
+{
+return(numbers[minus] == key || numbersPad[minus] == key);
+}
+//возвращает соответствующую символу цифру, иначе -1
+int GetDigit(ConsoleKey key)
+{
+for (int i = 0; i< 10; i++)
+{
+if (numbers[i] == key || numbersPad[i] == key)
+{
+return i;
+}
+}
+return -1;
+}
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+№43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+//==решение==
+int b1 = ReadData("Введите b1: ");
+int k1 = ReadData("Введите k1: ");
+int b2 = ReadData("Введите b2: ");
+int k2 = ReadData("Введите k2: ");
+
+double x = getLineCollisionX(b1,k1,b2,k2);
+double y = getLineY(b1,k1,b2,k2,x);
+
+prettyPrintResult(b1,k1,b2,k2,x,y);
+
+//==методы==
+
+// Печать результата в отформатированном виде
+void prettyPrintResult(int b1,int k1, int b2, int k2, double x, double y)
+{
+    
+    Console.WriteLine("b1 = {0}, k1 = {1}, b2 = {2}, k2 = {3} -> ({4};{5})",
+    b1,k1,b2,k2,x,y
+    );
+}
+
+// Решение Уравнения y = k1 * x + b1, y = k2 * x + b2 или k1 * x + b1 =  k2 * x + b2
+double getLineCollisionX(int b1,int k1, int b2, int k2)
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("Уравнение не имеет решения: k1 = k2 ({0} = {1})",k1,k2);
+        Environment.Exit(0);
+    }
+    return (double)(b2-b1)/(k1-k2);
+}
+
+// считаем y по любому из 2х уравнений 
+double getLineY(int b1,int k1, int b2, int k2, double x)
+{
+    return (double)(k1*x + b1);
+}
+
+// считываем ввод пользователя
+int ReadData(string message) 
+{
+    Console.Write(message);
+    string? value = Console.ReadLine();
+    int result = -1;
+    if (value == null)
+    {
+        Console.WriteLine("Пустое значение, выход");
+        Environment.Exit(0);
+    }
+      Boolean parsed =  int.TryParse(value,out result);
+    if (!parsed)
+    {
+        Console.WriteLine("Ошибка при определинии числа, выход");
+        Environment.Exit(0);
+    }
+    return result;
+}
